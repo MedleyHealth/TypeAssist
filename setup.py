30ced6d2a20abf6895c9ec2628babb6ea4f28bf1
Eric Yates <eric@medleyagency.com>
2020-09-17 01:44:10 -0600

    Configure package setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='type_assist',
+    version='0.0.1',
+    description='Model training and serving for predictive text completion',
+    license='Apache 2.0',
+    author='Eric Yates',
+    author_email='eric@medleyagency.com',
+    url='https://github.com/MedleyLabs/TypeAssist',
+    packages=['type_assist'],
+)
