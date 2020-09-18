311642f1c9e527aff508f30f8e8d33c70a8891e9
Eric Yates <eric@medleyagency.com>
2020-09-18 13:35:06 -0600

    Add README and LICENSE

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
+
 setup(
     name='type_assist',
     version='0.0.1',
     description='Model training and serving for predictive text completion',
-    license='Apache 2.0',
+    long_description=long_description,
+    license=license,
     author='Eric Yates',
     author_email='eric@medleyagency.com',
     url='https://github.com/MedleyLabs/TypeAssist',
